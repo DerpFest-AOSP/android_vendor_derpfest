77d57b0e6bb92cf0a0d5009144381fc33809ccf5
storm119 <storm119@gmail.com>
2023-04-01 02:17:11 +0530

    May the GOD bless my ancestors...
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

---
+++ config/derpfest.mk
@@ -0,0 +1,31 @@
+# DRM Service
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
+
+# Disable RescueParty due to high risk of data loss
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Blur
+ifeq ($(TARGET_NOT_USES_BLUR), false)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+endif
+
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
+# Enable dex2oat64 to do dexopt
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
