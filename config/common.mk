5167972673bfa6c3f3013c6045b91e039005a01d
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-05 00:47:26 +0530

    derpfest: Import lottie animations from caiman SettingsGoogle
    Change-Id: I622320692cc2cf820b2f17929fa204d65fdc1f56
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- config/common.mk
+++ config/common.mk
@@ -215,6 +215,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Overlay
+PRODUCT_PACKAGES += \
+    SettingsOverlayCustom
+
 # OverlayFS
 PRODUCT_PACKAGES_DEBUG += \
     disable-overlays
